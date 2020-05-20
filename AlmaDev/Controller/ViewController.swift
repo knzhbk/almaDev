92967f36e241a1981dc038208353de8c20f1b3b8
Ainurkz <git clone https://Ainurkz@bitbucket.org/mxbb/almadev.git>
2020-05-21 01:15:49 +0600

    Initial Commit

---
+++ AlmaDev/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlmaDev
+//
+//  Created by Apple on 5/21/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

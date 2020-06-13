44c0ff56c06b5e1bc1cb97235a0296266126822f
knzhbk <adina18022013@gmail.com>
2020-06-13 20:08:08 +0600

    Revert "Added isFirstLaunch function"
    This reverts commit d154ecbb4645261a9de0389d2e8c5b0359d2304a.

--- AlmaDev/Controller/LevelViewController.swift
+++ AlmaDev/Controller/LevelViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class LevelViewController: UIViewController {
+
     
     @IBOutlet var levelLabel: UILabel!
     @IBOutlet weak var loadingBar: UIActivityIndicatorView!
@@ -20,53 +21,41 @@ class LevelViewController: UIViewController {
     var questionAddress = ""
     var cat: String = ""
     var questionProvider: QuestionProvider?
-    var levelChanged = false
     
+    var levelChanged = false
+
+
     @IBOutlet weak var categoryLabel: UILabel!    
     override func viewDidLoad() {
         super.viewDidLoad()
+        levelLabel.text = UserDefaults.standard.string(forKey: "Level")
         startButton.alpha = 0
         Utilities.startButton(startButton)
         
-        if isFirstLaunch() {
-            UserDefaults.standard.set("Easy", forKey: "Level")
-            levelLabel.text = UserDefaults.standard.string(forKey: "Level")
-        } else {
-            levelLabel.text = UserDefaults.standard.string(forKey: "Level")
-        }
-        
+    
         NotificationCenter.default.addObserver(self, selector: #selector(settingLevel), name: NOTIFICATION_SETTING_LEVEL, object: nil)
         
         
-        if levelChanged == false {
+        if levelChanged == false
+        {
             loadQuestions()
         }
-    }
+
+        }
     
-    func loadQuestions() {
+    func loadQuestions()
+    {
         if let category = category {
             categoryLabel.text = category.categoryName
-            NotificationCenter.default.addObserver(self, selector: #selector(showButton), name: NOTIFICATION_QUESTION_SEND, object: nil)
-            
+
+                 NotificationCenter.default.addObserver(self, selector: #selector(showButton), name: NOTIFICATION_QUESTION_SEND, object: nil)
+        
             questionProvider = QuestionProvider()
-            questionAddress = "https://opentdb.com/api.php?amount=5&category=\(category.categoryNum!)&difficulty=\(level.lowercased())&type=multiple"
-            questionProvider?.getQuestion(questionAddress: questionAddress)
+        questionAddress = "https://opentdb.com/api.php?amount=5&category=\(category.categoryNum!)&difficulty=\(level.lowercased())&type=multiple"
+                       questionProvider?.getQuestion(questionAddress: questionAddress)
+            
         }
     }
-    
-    func isFirstLaunch() -> Bool {
-        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
-        if launchedBefore  {
-            print("Not first launch.")
-            return false
-        }
-        else {
-            print("First launch, setting NSUserDefault.")
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
-            return true
-        }
-    }
-    
     @objc func settingLevel(notification: Notification){
         if let setLevel = notification.object as?  String
         {
@@ -75,13 +64,17 @@ class LevelViewController: UIViewController {
             level = setLevel
             levelLabel.text = level
             loadQuestions()
+            
+            
+            
         }
     }
     @objc func showButton(){
         loadingLabel.alpha = 0
         loadingBar.alpha = 0
         startButton.alpha = 1
-    }
+         }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destination = segue.destination as? TestViewController
@@ -90,3 +83,4 @@ class LevelViewController: UIViewController {
         print("INFO: \(questionAddress)")
     }
 }
+

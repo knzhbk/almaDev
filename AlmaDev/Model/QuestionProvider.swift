7ddd6240d0b49a879e3b8a378bbe817aec08bc11
Adina Kenzhebekova <adina18022013@gmail.com>
2020-06-15 09:17:38 +0000

    Merged in makeGetfaster (pull request #17)
    QuestionLoadsFaster

--- AlmaDev/Model/QuestionProvider.swift
+++ AlmaDev/Model/QuestionProvider.swift
@@ -19,15 +19,15 @@ class QuestionProvider {
                    if let data = data {
                        do {
                            self.question = try JSONDecoder().decode(Question.self,from: data)
-                     //       print("Has send")
+                            DispatchQueue.main.async {
                         let ref = Database.database().reference()
-                       // let userID = Auth.auth().currentUser!.uid
                         var i = 0
                         for value in self.question!.results {
                             ref.child("User01").child("\(i)").setValue(["category": value.category, "question": value.question,"correct_answer": value.correct_answer,"incorrect_answers0": value.incorrect_answers[0],"incorrect_answers1": value.incorrect_answers[1],"incorrect_answers2": value.incorrect_answers[2]])
                             i += 1
                         }
                         NotificationCenter.default.post(name: NOTIFICATION_QUESTION_SEND, object: self.question )
+                        }
 
                        } catch {
                            print("Decoding JSON failure")
